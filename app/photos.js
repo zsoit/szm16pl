85567b31156b7829d69decd43000c4e7602945aa
zsoit <wsparcie.exe@gmail.com>
2020-08-24 21:02:45 +0200

    Update 3

--- app/photos.js
+++ app/photos.js
@@ -2,5 +2,5 @@ function Photos() {
     document.getElementById("main__content").innerHTML = "<h2>Galeria</h2><center><img class=photos__item src=img/foto3.jpg> <img class=photos__item src=img/foto4.jpg></center>";
 
     document.title = "Galeria";
-   
+
 }
