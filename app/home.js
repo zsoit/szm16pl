11227641f4340cd3187c82067d0fa3d843b27528
Jakub Achtelik <wsparcie.exe@gmail.com>
2020-08-24 19:57:52 +0200

    Add files

---
+++ app/home.js
@@ -0,0 +1,10 @@
+function Home() {
+    document.getElementById("main__content").innerHTML = "<h2>O mnie</h2><div class=wrapper__home><img class=home__avatar src=img/szymon.png><p>Nazywam się Szymon Monasterski i na codzień zajmuję się usługami informatycznymi. Potrafię naprawić komputer, smartfon skonfigurować sieć, urządzenia elektorniczne. Obecnie uczęszczam do technikum informatycznego, gdzie uczę zawodu.</div>";
+    document.title = "Strona Główna";
+   
+}
+
+/* <h2>O mnie</h2> */
+
+
+
---
+++ app/price.js
@@ -0,0 +1,4 @@
+function Price() {
+    document.getElementById("main__content").innerHTML = "<h2>Cennik</h2>";
+    document.title = "Cennik";
+}
