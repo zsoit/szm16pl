5c540058bba79d14390b55ef7b51fb272036bab3
zsoit <wsparcie.exe@gmail.com>
2020-08-25 11:22:27 +0200

    Update ++

--- app/photos.js
+++ app/photos.js
@@ -1,5 +1,5 @@
 function Photos() {
-    document.getElementById("main__content").innerHTML = "<h2>Galeria</h2><img height=250 name=slide width=400>";
+    document.getElementById("main__content").innerHTML = "<h2>Galeria</h2>";
 
     document.title = "Galeria";
 
