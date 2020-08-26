a5cea63acb136952c21c6efb3179612ef78347d8
jakun <wsparcie.exe@gmail.com>
2020-08-26 11:13:18 +0200

    update with slider

--- app/slider.js
+++ app/slider.js
@@ -1,24 +1,30 @@
-var i = 0;
-var images = [];
-var time = 500;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-
-images[0] = 'img/image1-min.jpg';
-images[1] = 'img/image2-min.jpg';
-images[2] = 'img/image3-min.jpg';
-images[3] = 'img/image4-min.jpg';
-
-
-function changeImg() {
-    document.slide.src = images[i];
-
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-
-    setTimeout("changeImg()", time);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-window.onload = changeImg;
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
