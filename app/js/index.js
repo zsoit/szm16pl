8ef7cb305204fbb1e017ed66d7805ad7a61075ba
zsoit <wsparcie.exe@gmail.com>
2021-01-06 17:43:27 +0100

    Minifity

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,22 @@
+document.querySelector('#price').addEventListener('click', event => {
+    Price();
+})
+
+document.querySelector('#contact').addEventListener('click', event => {
+    Contact();
+})
+
 function Price() {
-    document.getElementById("main__content").innerHTML = "<h2>Oferta</h2><div class=list><ul><li>Instalacje systemów operacyjnych oraz sterowników<li>Montaż podzespołów komputerowych<li>Serwisowanie, naprawa komputera<li>aktualizowanie Biosu<li>Konfiguracja sieci domowej<li>Serwisowanie smartfonów<li>Zarabianie gniazd internetowych<li>Zarabianie patch paneli oraz keystone<li>Wymiana przycisków i analogów w padach<li>Konfiguracja obrazu w TV do korzystania codziennego<li>Instalacja Monitoringu</ul></div> <br><br>";
+    document.getElementById("main__content").innerHTML = `
+    <h2>Oferta</h2>
+    <div class=list>
+    <ul>
+    <li>Instalacje systemów operacyjnych oraz sterowników<li>
+    Montaż podzespołów komputerowych<li>Serwisowanie, naprawa komputera<li>aktualizowanie Biosu<li>Konfiguracja sieci domowej<li>Serwisowanie smartfonów<li>Zarabianie gniazd internetowych<li>Zarabianie patch paneli oraz keystone<li>Wymiana przycisków i analogów w padach<li>Konfiguracja obrazu w TV do korzystania codziennego<li>Instalacja Monitoringu</ul></div> <br><br>`;
     document.title = "Oferta";
 
     const oferta = document.getElementById("oferta");
-    const kontakt = document.getElementById("contact");
+    const kontakt = document.getElementById("kontakt");
     const omnie = document.getElementById("omnie");
 
     kontakt.style.color = "white";
@@ -19,11 +32,11 @@ function Price() {
 }
 
 function Contact() {
-    document.getElementById("main__content").innerHTML = "<h2>Kontakt</h2><div class=contact><p class=contact__name>SZM16 POMOC IT</p><a href=tel:+48510962288><p class=contact__phone>+48 510 962 288</p></a><a href=mailto:monasterski.it@gmail.com><p class=contact__mail>monasterski.it@gmail.com</p></a><p>Wycena oraz dodatkowe informacje udzielane indywidualnie.</div>";
+    document.getElementById("main__content").innerHTML = `<h2>Kontakt</h2><div class=contact><p class=contact__name>SZM16 POMOC IT</p><a href=tel:+48510962288><p class=contact__phone>+48 510 962 288</p></a><a href=mailto:monasterski.it@gmail.com><p class=contact__mail>monasterski.it@gmail.com</p></a><p>Wycena oraz dodatkowe informacje udzielane indywidualnie.</div>`;
     document.title = "Kontakt";
 
     const oferta = document.getElementById("oferta");
-    const kontakt = document.getElementById("contact");
+    const kontakt = document.getElementById("kontakt");
     const omnie = document.getElementById("omnie");
 
 
